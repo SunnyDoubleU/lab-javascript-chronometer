d003aa316aa29b510164aab5977a53ef761b43dc
Sunny Wong <sunnywong18@gmail.com>
2019-09-11 19:36:24 +0200

    Completed chronometer exercise

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,59 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.intervalId = ""
+    this.currentTime = 0 // this second
+  }
+  startClick() {
+    var fixThis = this
+    // save the interval ref somewhere in this object
+    fixThis.intervalId = setInterval(function() {
+      fixThis.currentTime++
+      // printTime()
+    }, 1000)
+  }
+ 
+getMinutes() {
+  var fixThis = this
+  fixThis.minNum = 0
+  fixThis.minNum = Math.floor(fixThis.currentTime / 60)
+  return fixThis.minNum
+  }
+
+getSeconds() {
+  var fixThis = this 
+  var minNum = Math.floor(fixThis.currentTime / 60)
+  fixThis.secNum = 0
+  fixThis.secNum = fixThis.currentTime - (minNum * 60)
+  return fixThis.secNum
+}
+
+getMilli() {
+}
+
+twoDigitsNumber(num) {
+  this.twoDigNum = ""
+
+  if (num < 10) {
+    this.twoDigNum = `0${num}` 
+  }
+  else {
+    this.twoDigNum = `${num}`
+  } 
+
+  return this.twoDigNum
+}
+
+stopClick() {
+  clearInterval(this.intervalId)
+}
+
+resetClick() {
+  this.currentTime = 0
+}
+splitClick() {
+  this.minute = `${this.twoDigitsNumber(this.getMinutes())}`
+  this.second = ` ${this.twoDigitsNumber(this.getSeconds())}`
+  this.timeNow = `${this.minute}:${this.second}`
+  return this.timeNow
+}
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -7,54 +7,105 @@ var secDec      = document.getElementById('secDec');
 var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
+var splits      = document.getElementById('splits');
 
 
 function printTime() {
-
+    printMinutes()
+    printSeconds()
 }
 
 function printMinutes() {
-
+    function minuteMark() {
+        minDec.innerHTML = `${chronometer.twoDigitsNumber(chronometer.getMinutes())[0]}`
+        minUni.innerHTML = `${chronometer.twoDigitsNumber(chronometer.getMinutes())[1]}`
+    }
+ setInterval(minuteMark, 1000)
 }
 
 function printSeconds() {
-
+    function secondMark() {
+        secDec.innerHTML = `${chronometer.twoDigitsNumber(chronometer.getSeconds())[0]}`
+        secUni.innerHTML = `${chronometer.twoDigitsNumber(chronometer.getSeconds())[1]}`
+    }
+setInterval(secondMark, 1000)
 }
 
-function printMilliseconds() {
 
+function printMilliseconds() {
 }
 
 function printSplit() {
+    let splitList = document.createElement("li")
+    splitList.innerHTML = `${chronometer.splitClick()}`
 
+    splits.appendChild(splitList)
 }
 
 function clearSplits() {
-
+   splits.innerHTML = ""
 }
 
 function setStopBtn() {
+    // as soon as click start, reset the set split btn 
+btnLeft.innerHTML = "STOP"
+btnLeft.classList.remove("start")
+btnLeft.classList.add("stop")
 
 }
 
 function setSplitBtn() {
-
+btnRight.innerHTML = "SPLIT"
+btnRight.classList.remove("reset")
+btnRight.classList.add("split")
 }
 
 function setStartBtn() {
+btnLeft.innerHTML = "START"
+btnLeft.classList.remove("stop")
+btnLeft.classList.add("start")
 
 }
 
 function setResetBtn() {
-
+btnRight.innerHTML = "RESET"
+btnRight.classList.remove("split")
+btnRight.classList.add("reset")
 }
+
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+
+if (btnLeft.innerHTML ==="START") {
+setStopBtn()
+setSplitBtn()
+chronometer.startClick()
+printTime()
+}
+else {
+setStartBtn()
+setResetBtn()
+chronometer.stopClick()
+}
 
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
 
+if (btnRight.innerHTML === "RESET"){
+setSplitBtn()
+clearSplits()
+chronometer.resetClick()
+setStartBtn()
+setResetBtn()
+
+}
+else {
+// setResetBtn()
+// setStartBtn()
+printSplit()
+}
 });
+
